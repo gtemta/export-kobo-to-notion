d6e921a9a924ec5c57f2653e336475f22930b9f1
Yao <anniehsu32@gmail.com>
2023-06-17 11:10:07 +0800

    /#	modified:   index.js
    1. Use local DB to create new book  columns
    2. Synced note's will be skip for updating

--- index.js
+++ index.js
@@ -14,32 +14,53 @@ async function exportHighlights() {
 
   for (book of bookList) {
     try {
+      console.log(`Get ${book.Title} from DB`)
       // Removes subtitles from book title
       if (book.Title.indexOf(":") !== -1) {
+        console.log(`${book.Title} Original Book Title`)
         book.Title = book.Title.substring(0, book.Title.indexOf(":"));
+        console.log(`${book.Title} Shrink Book Title`)
       }
       let title = book.Title;
-
-      // Check Notion database for the book
-      const response = await notion.databases.query({
-        database_id: NOTION_DATABASE_ID,
-        filter: {
-          and: [
-            { property: "Title", text: { contains: title } },
-            { property: "Highlights", checkbox: { equals: false } },
-          ],
-        },
-      });
+      //Check Notion database for the book
+      // let response = await notion.databases.query({
+      //   database_id: NOTION_DATABASE_ID,
+      //   filter: {
+      //     and: [
+      //       { property: "Title", text: { contains: title } },
+      //       { property: "Exported", checkbox: { equals: false } },
+      //     ],
+      //   },
+      // });
+      const syncedTarget = querySyncedfromDB(title);
+      if () {
+        
+      }
+      let unSyncedTarget = queryUnsyncfromDB(title);
 
       // Use the results to determine status of the book
       var valid = false;
-      if (response.results.length === 1) {
+      const unSyncFound = (unSyncedTarget.results.length === 1);
+      if (unSyncFound) {
         valid = true;
-      } else if (response.results.length > 1) {
-        console.log(`${title} matched multiple items.`);
+      } else if (syncedTarget) {
+        onsole.log(`${title} synced. Skip Update Notes`);
       } else {
-        console.log(`${title} was skipped.`);
+        console.log(`${title} unexisted. Try add Entry for it`);
+        valid = addEntryByTitle(title);
       }
+      if (valid != true && syncedTarget == false) {
+        console.log(`Try Get Again after wait 2 second`);
+        await sleep(2000);
+        //Check Notion database for the book
+        let unSyncedTarget = queryUnsyncfromDB(title);
+        const unSyncAdded = (unSyncedTarget.results.length === 1);
+        if (unSyncAdded){
+          valid = true;
+        }
+      }
+      valid = false;
+
 
       if (valid) {
         const pageId = response.results[0].id;
@@ -74,6 +95,16 @@ async function exportHighlights() {
               },
             });
           }
+          if (blocks.length > 90) {
+              console.log(`Over ${blocks.length} append children first`);
+              // Appends the blocks to the book page
+              await notion.blocks.children.append({
+                block_id: pageId,
+                children: blocks,
+              });            
+              blocks = [];
+              console.log(`Clear Block to ${blocks.length}`);
+          }
         }
 
         // Appends the blocks to the book page
@@ -85,7 +116,7 @@ async function exportHighlights() {
         // Updates the status of the book page
         await notion.pages.update({
           page_id: pageId,
-          properties: { Highlights: { checkbox: true } },
+          properties: { Exported: { checkbox: true } },
         });
 
         console.log(`Uploaded highlights for ${title}.`);
@@ -94,6 +125,92 @@ async function exportHighlights() {
       console.log(`Error with ${book.Title}: `, error);
     }
   }
+}
+async function addEntryByTitle(bookTitle) {
+  try {
+    // Create a new entry in the Notion database
+    const response = await notion.pages.create ({
+      parent: {
+        database_id: NOTION_DATABASE_ID
+      },
+      properties: {
+        title: {
+          title: [
+            {
+              text: {
+                content: bookTitle
+              }
+            }
+          ]
+        }
+      }
+    });
+    console.log(`Entry ${bookTitle} added successfully!`);
+    return true;
+  } catch (error) {
+    console.error('Error adding entry:', error.message);
+    return false;
+  }
+}
+
+async function queryUnsyncfromDB(bookTitle) {
+  let hasOnly = false;
+  try {
+    // Create a new entry in the Notion database
+    const response = await notion.databases.query({
+        database_id: NOTION_DATABASE_ID,
+        filter: {
+          and: [
+            { property: "Title", text: { contains: bookTitle } },
+            { property: "Exported", checkbox: { equals: false } },
+          ],
+        },
+    });
+    if (response.results.length === 1) {
+      console.log(`Unsynced ${bookTitle} only one found.`);
+      hasOnly = true;
+    } else if (response.results.length > 1) {
+      console.log(`Unsynced ${bookTitle} matched multiple items.`);
+    } else {
+      console.log(`Unsynced ${bookTitle} unexisted.`);
+    }
+    return response;
+  } catch (error) {
+    console.error('Error query[queryUnsyncfromDB]:', error.message);
+  }
+}
+
+async function querySyncedfromDB(bookTitle) {
+  let hasOnly = false;
+  try {
+    // Create a new entry in the Notion database
+    const response = await notion.databases.query({
+        database_id: NOTION_DATABASE_ID,
+        filter: {
+          and: [
+            { property: "Title", text: { contains: bookTitle } },
+            { property: "Exported", checkbox: { equals: true } },
+          ],
+        },
+    });
+    if (response.results.length === 1) {
+      console.log(`Synced ${bookTitle} only one found.`);
+      hasOnly = true;
+    } else if (response.results.length > 1) {
+      console.log(`Synced ${bookTitle} matched multiple items.`);
+    } else {
+      console.log(`Synced ${bookTitle} unexisted.`);
+    }
+    return hasOnly;
+  } catch (error) {
+    console.error('Error query [querySyncedfromDB]:', error.message);
+  }
+}
+
+function sleep(ms) {
+  return new Promise(resolve => {
+    setTimeout(resolve, ms);
+  });
 }
 
 exportHighlights();
